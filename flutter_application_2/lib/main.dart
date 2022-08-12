1347280bf37ea9f444f1f95ddd8b0c54b16d8b8f
Abid <abid@inverseais-MacBook-Air.local>
2022-08-12 11:44:09 +0600

    init

---
+++ flutter_application_2/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_2/pages/login_page.dart';
+
+void main() => runApp(
+  const MaterialApp(
+    title: 'Flutter Application 2',
+    home: LoginPage(),
+  ),
+);
