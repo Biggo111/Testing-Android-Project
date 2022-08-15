bef326ca9b88674e5b4942bd440e12fc79c7fa69
Abid <abid@inverseais-MacBook-Air.local>
2022-08-15 20:00:32 +0600

    score system added

--- word_ladder_game/lib/main.dart
+++ word_ladder_game/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+// declare a list of unique strings
+List<String> kStrings = <String>[];
+int attempts = 3;
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
@@ -21,6 +24,7 @@ class Game extends StatefulWidget {
   const Game({Key? key}) : super(key: key);
 
   @override
+  // ignore: library_private_types_in_public_api
   _GameState createState() => _GameState();
 }
 
@@ -39,8 +43,43 @@ class _GameState extends State<Game> {
   );
   void _submit() {
     setState(() {
-      _controller2.text = '${_controller2.text}${controller.text} ';
-      String firstChar = controller.text.substring(0, 1).toUpperCase();
+      String word = controller.text;
+      if(kStrings.contains(word))
+      {
+        attempts--;
+        if(attempts == 0)
+        {
+          // show dialog box "Game Over"
+          controller.clear();
+          _controller2.clear();
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: const Text('Game Over'),
+                content: const Text('You have run out of attempts'),
+                actions: <Widget>[
+                  TextButton(
+                    child: const Text('OK'),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              );
+            },
+          );
+          return;
+        }
+        decoration = InputDecoration(
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+          hintText: 'Enter a word',
+          errorText: 'Word already used!',
+        );
+      }
+      kStrings.add(word);
+      _controller2.text = '${_controller2.text}$word ';
+      String firstChar = word.substring(0, 1).toUpperCase();
       if (lastChar != null && lastChar != firstChar) {
         lastChar = null;
         _controller2.clear();
@@ -50,8 +89,8 @@ class _GameState extends State<Game> {
           errorText: 'Words must start with the same letter',
         );
         return;
-      } 
-      lastChar = controller.text.characters.last.toUpperCase();
+      }
+      lastChar = word.characters.last.toUpperCase();
       decoration = InputDecoration(
         border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
         hintText: 'Enter a word starts with $lastChar',
@@ -66,27 +105,39 @@ class _GameState extends State<Game> {
       child: Column(
         children: [
           const Padding(padding: EdgeInsets.all(8)),
-          TextField(
-            controller: _controller2,
-            enabled: false,
-            decoration: InputDecoration(
-              contentPadding: const EdgeInsets.all(20),
-              labelText: 'Your guessed words',
-              fillColor: Colors.limeAccent,
-              border:
-                  OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+          Container(
+            padding: const EdgeInsets.all(8),
+            child: TextField(
+              style: TextStyle(
+                height: MediaQuery.of(context).size.height*0.01,
+              ),
+              controller: _controller2,
+              enabled: false,
+              scrollController: ScrollController(
+                initialScrollOffset: 0,
+              ),
+              decoration: InputDecoration(
+                
+                labelText: 'Your guessed words',
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+              ),
             ),
           ),
           const Padding(padding: EdgeInsets.all(10)),
-          TextField(
-            controller: controller,
-            onSubmitted: (value) => _submit(),
-            autofocus: true,
-            focusNode: _focusNode,
-            // focus back to the text field after submission
-            onEditingComplete: () =>
-                FocusScope.of(context).requestFocus(_focusNode),
-            decoration: decoration,
+          Container(
+            width: MediaQuery.of(context).size.width * 0.6,
+            alignment: Alignment.center,
+            child: TextField(
+              controller: controller,
+              onSubmitted: (value) => _submit(),
+              autofocus: true,
+              focusNode: _focusNode,
+              // focus back to the text field after submission
+              onEditingComplete: () =>
+                  FocusScope.of(context).requestFocus(_focusNode),
+              decoration: decoration,
+            ),
           ),
           ElevatedButton(onPressed: _submit, child: const Text('Go')),
         ],
